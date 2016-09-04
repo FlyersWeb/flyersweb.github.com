a6d7bf8b9cc5eec88e317153ae5ce7b112642447
FlyersWeb <contact@flyers-web.org>
2016-09-04 23:54:38 +0200

    Cleaned imports

--- src/component/presentational/home/home.jsx
+++ src/component/presentational/home/home.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import { Row, Col } from 'react-bootstrap';
-
 import Jumbotron from '../jumbotron/customjumbotron.jsx'
 import Footer from '../footer/footer.jsx'
 
--- src/component/presentational/project/project.jsx
+++ src/component/presentational/project/project.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
-import { Row, Col } from 'react-bootstrap';
-
 import Jumbotron from '../jumbotron/customjumbotron.jsx'
 import Footer from '../footer/footer.jsx'
 
