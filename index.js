614a5338422b6a1ebe8b6316de1a072d6a2f68d0
FlyersWeb <contact@flyers-web.org>
2017-01-21 14:17:37 +0100

    Add blog redirections

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+var path = require("path");
+var url = require("url");
 var express = require('express');
 var app = express();
 
-app.use('/assets', express.static('bin'));
+var blog = "https://flyers-web.blogspot.fr/"
+
+app.use("/assets", express.static('bin'));
+
+app.get("/:year([0-9]{4})/:month([0-9]{2})/:slug([\\s\\S]+)$", function(req, res) {
+  var destPath = path.join(req.params.year, req.params.month, req.params.slug);
+  var destination = url.parse(blog + destPath);
+  res.redirect(301, url.format(destination))
+});
+
 app.get("/*", function(req, res) {
   res.sendFile(__dirname + '/index.html')
 });
