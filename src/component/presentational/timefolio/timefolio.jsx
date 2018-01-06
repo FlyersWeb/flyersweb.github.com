d863069dab10d05e8ce2facca361d1f85eae8d28
FlyersWeb <contact@flyers-web.org>
2018-01-06 01:31:07 +0100

    Added ouicar to timeline

--- src/component/presentational/timefolio/timefolio.jsx
+++ src/component/presentational/timefolio/timefolio.jsx
@@ -29,14 +29,27 @@ const Timefolio = React.createClass({
         <div className="container">
           <Jumbotron
             title="Career Timeline"
-            content={`I am a professional web developer, manager and architect. I'm focused on cutting edge technologies that match the on-demand project needs. 
+            content={`I am a professional web developer, manager and architect. I'm focused on cutting edge technologies that match the on-demand project needs.
             You'll find in this timefolio my pasts professional experiences so you'll have an idea of my practical skills that may match with your needs.`}
           />
-          <Timeline 
-            reverseOrder={true} 
+          <Timeline
+            reverseOrder={true}
             customTextBody={CustomBody}
             customFooter={CustomFooter}
             events={[
+							{
+								date: moment('2017-04-20'),
+								title: 'FullStack Developer @OuiCar',
+								text: `
+- Website improvements and refactoring
+- API Gateway for front ends
+- Search Engine refactoring
+
+
+> ReactJS, NodeJS, GraphQL, ElasticSearch, ES6, Git.
+								`,
+								imageUrl: require("../../../assets/logos/ouicar.png")
+							},
               {
                 date: moment('2016-04-20'),
                 title: 'FullStack Developer @ClubMed',
@@ -70,7 +83,7 @@ const Timefolio = React.createClass({
                 text: `
 - Research and Developments for new features
 - Analytics platform improvements and level 3 support
-- Interfacing with others Analytics platform like Google Adwords or Facebook 
+- Interfacing with others Analytics platform like Google Adwords or Facebook
 - Features Testings and Acceptances
 - Time based series spam detection
 
@@ -81,7 +94,7 @@ const Timefolio = React.createClass({
               },
               {
                 date: moment('2012-12-01'),
-                title: 'Project Manager @DresdenPartners',            
+                title: 'Project Manager @DresdenPartners',
                 text: `
 - Clients Definition of needs
 - Project scoping
@@ -90,23 +103,23 @@ const Timefolio = React.createClass({
 - 5 person team management
 
 > AngularJS, BackboneJS, Jquery, Sass and Grunt
-> 
+>
 > PHP/Symfony, Javascript/NodeJS, C#/.Net
-> 
-> Nodejitsu, Amazon EC2, ElasticBeanStalk 
+>
+> Nodejitsu, Amazon EC2, ElasticBeanStalk
 
 
 Project examples :
-- Eweclid: Mobile learning application with 
-- Evamerc : Web marketing dashboard 
+- Eweclid: Mobile learning application with
+- Evamerc : Web marketing dashboard
 - ADOMovil : Scoping web api needs for the mobile application
-- Sensis Agency : Events landing pages 
+- Sensis Agency : Events landing pages
                 `,
                 imageUrl: require("../../../assets/logos/dresden.png")
               },
               {
                 date: moment('2012-03-01'),
-                title: 'Web Developer @TikiMove',            
+                title: 'Web Developer @TikiMove',
                 text: `
 - Project management web application using JQuery and JQueryUI
 - Web services for events mobile applications
