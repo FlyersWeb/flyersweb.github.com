f31713c13d980aaef3d1b674f134bce7881c350a
Flyers <contact@flyers-web.org>
2018-02-25 01:17:29 +0100

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-4d12bca6326e9158e4c4.js","24a564a855caef30008908b7cec236cb"],["/app-576dde739987df4be98a.js","8638361ab84865da8b2924a931f09504"],["/app-f1ec8545bcc9962cb1d8.js","633128aaca9c27b5870dd515b41a3354"],["/commons-21ae682480863ffb56da.js","1d2eb104191f497bd6c5558a7e49f511"],["/commons-e25bc4f53087b82959aa.js","8d89bda11563116a8e72909db4193178"],["/commons-eb19b7d7ca390cf13f03.js","ec91e287187e16a4d30fd0c8c1246295"],["/index.html","f7d57ea0a9588d600dd7c2fbbde6d0a3"],["/manifest.json","666187aa2774d28ddc7ce0d1333f483a"],["/offline-plugin-app-shell-fallback/index.html","be4017c3ae72f3bd026df013014ede3c"]];
+var precacheConfig = [["/app-4d12bca6326e9158e4c4.js","24a564a855caef30008908b7cec236cb"],["/app-576dde739987df4be98a.js","8638361ab84865da8b2924a931f09504"],["/app-f1ec8545bcc9962cb1d8.js","633128aaca9c27b5870dd515b41a3354"],["/commons-21ae682480863ffb56da.js","1d2eb104191f497bd6c5558a7e49f511"],["/commons-45aff1ac7c189fc21489.js","40580b1b1b11eb596fe97ba12d7f3988"],["/commons-e25bc4f53087b82959aa.js","8d89bda11563116a8e72909db4193178"],["/commons-eb19b7d7ca390cf13f03.js","ec91e287187e16a4d30fd0c8c1246295"],["/index.html","d19d5a70a3b8b0d77358b736cbbe45a8"],["/manifest.json","666187aa2774d28ddc7ce0d1333f483a"],["/offline-plugin-app-shell-fallback/index.html","e1b7ac0d0a52152de3201f52c0b39e61"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
