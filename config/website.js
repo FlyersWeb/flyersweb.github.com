6d49bba6f8a50a6643fbe9a35992bf859ed1a2f8
FlyersWeb <contact@flyers-web.org>
2018-10-10 22:08:56 +0200

    gatsby 2 migration

---
+++ config/website.js
@@ -0,0 +1,22 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+  siteTitle: 'Flyers Web Developer', // Navigation and Site Title
+  siteTitleAlt: 'Nassim Ben Ghmiss Web Developer Portfolio', // Alternative Site title for SEO
+  siteUrl: 'https://www.flyersweb.org', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
+  siteDescription: 'Showcase my web projects for my different clients',
+  author: 'Nassim', // Author for schemaORGJSONLD
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@flyersweb', // Twitter Username
+  // ogSiteName: 'emma', // Facebook Site Name
+  ogLanguage: 'en_US',
+  // googleAnalyticsID: 'UA-12345689-1',
+  // Manifest and Progress color
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
+  // Settings for typography.jsx
+  headerFontFamily: 'Merriweather',
+  bodyFontFamily: 'Roboto',
+  baseFontSize: '16px',
+};
